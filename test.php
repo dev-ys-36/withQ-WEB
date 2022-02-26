4d943a5c6bfe017860868123738b67794bf0eed5
dev-ys-36 <98698629+dev-ys-36@users.noreply.github.com>
2022-02-27 01:43:32 +0900

    Add files via upload

--- test.php
+++ test.php
@@ -6,8 +6,8 @@ if (date_default_timezone_get() != 'Asia/Seoul'){
 
 include './vendor/autoload.php';
 include './HttpServerManager.php';
-include './MainLogger.php';
 include './utils/DataManager.php';
+include './utils/MainLogger.php';
 
 use React\Http\HttpServer;
 use React\Http\Message\Response;
@@ -15,8 +15,9 @@ use React\Socket\SocketServer;
 
 use Psr\Http\Message\ServerRequestInterface;
 
+$logger = new utils\MainLogger(__DIR__);
 
-$logger = new MainLogger();
+# https -----------------------------------------------------------------------------------------------------
 
 $https = new HttpServer(
 	function(ServerRequestInterface $request): Response{
@@ -24,24 +25,30 @@ $https = new HttpServer(
 		return $manager->onRequest();
 	}
 );
+
 $uri = 'tls://0.0.0.0:443';
+$fullchain = '/etc/letsencrypt/live/withq.kr/fullchain.pem';
+$privkey = '/etc/letsencrypt/live/withq.kr/privkey.pem';
+
 $httpsSocket = new SocketServer(
 	$uri, [
 		'tls' => [
-			'local_cert' => '/etc/letsencrypt/live/withq.kr/fullchain.pem',
-			'local_pk' => '/etc/letsencrypt/live/withq.kr/privkey.pem',
+			'local_cert' => $fullchain,
+			'local_pk' => $privkey,
 			'verify_peer' => FALSE
 		]
 	]
 );
+
 $https->listen($httpsSocket);
 
 $httpsSocket->on('error', function(Exception $e){
-	$logger::text('HTTPS SERVER FAIL, ' . $e->getMessage());
+	$logger::text($logger::COLOR_RED . 'HTTPS SERVER FAIL, ' . $e->getMessage() . $logger::FORMAT_RESET);
 });
 
 $logger::text($logger::COLOR_GREEN . 'HTTPS SERVER SUCCESS' . $logger::FORMAT_RESET);
 
+# http -----------------------------------------------------------------------------------------------------
 
 $http = new HttpServer(
 	function(ServerRequestInterface $request): Response{
@@ -50,10 +57,11 @@ $http = new HttpServer(
 );
 
 $httpSocket = new SocketServer('0.0.0.0:80');
+
 $http->listen($httpSocket);
 
 $httpSocket->on('error', function(Exception $e){
-	$logger::text('HTTP SERVER FAIL, ' . $e->getMessage());
+	$logger::text($logger::COLOR_RED . 'HTTP SERVER FAIL, ' . $e->getMessage() . $logger::FORMAT_RESET);
 });
 
 $logger::text($logger::COLOR_GREEN . 'HTTP SERVER SUCCESS' . $logger::FORMAT_RESET);
